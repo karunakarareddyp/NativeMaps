3d918b04e3ed009bd40285c5861dfcd548bc1a62
karunakara-potchimireddy <karunakara.potchimireddy@hpe.com>
2018-12-20 23:01:27 +0530

    mapdb server config

--- server/db.js
+++ server/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-    DB: 'mongodb://@localhost:27017/TrackingData',
-    // DB: 'mongodb://@202.62.71.90:27017/TrackingData',
+    // DB: 'mongodb://@localhost:27017/TrackingData',
+    DB: 'mongodb://@202.62.71.90:27017/TrackingData',
 };
--- server/server.js
+++ server/server.js
@@ -5,15 +5,15 @@ const config = require('./db');
 
 const maps = require('./routes/maps');
 
-mongoose.connect(config.DB, { useNewUrlParser: true }).then(
-/*mongoose.connect(config.DB,
+//mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+mongoose.connect(config.DB,
     {
         auth: {
             user:'gpsdevadmin',
             password: 'Admin@2018'
         },
         useNewUrlParser: true
-    }).then(*/
+    }).then(
     () => { console.log('Successfully connected to Database'); },
     (err) => { console.log(`Can not connect to the database ${err}`); },
 );
